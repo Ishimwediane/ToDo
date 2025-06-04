1c940592acc55ab4d3ea6347a439148914ea75f3
Ishimwediane <ishimwediane400@gmail.com>
2025-06-04 10:43:56 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // This ensures assets are loaded relative to the current path
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
-  },
+  plugins: [react(),tailwindcss(),],
 })
